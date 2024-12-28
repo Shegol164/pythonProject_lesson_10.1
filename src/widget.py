becfd1572a3c018f9ed5c6950fc53d5aa0bf0107
unknown < >
2024-12-28 13:39:27 +0400

    add widget

--- src/widget.py
+++ src/widget.py
@@ -14,14 +14,14 @@ def mask_account_card(card_or_account: str) -> [str]:
     card_or_account_type = " ".join(card_or_account_splitted[:-1])
 
     if card_or_account.lower().startswith("счет"):
-        print(f"{card_or_account_type} {get_mask_account(card_or_account_splitted[-1])}")
+        return f"{card_or_account_type} {get_mask_account(card_or_account_splitted[-1])}"
     else:
-        print(f"{card_or_account_type} {get_mask_card_number(card_or_account_splitted[-1])}")
+        return f"{card_or_account_type} {get_mask_card_number(card_or_account_splitted[-1])}"
 
 
 def get_date(date: str) -> str:
     """Функция принимает на вход дату в формате 2024-03-11T02:26:18.671407 и возвращает в формате 11.03.2024"""
 
     now = datetime.datetime.strptime(date, "%Y-%m-%dT%H:%M:%S.%f")
-    new_data = now.strftime("%d.%m.%YT%H:%M:%S.%f")
+    new_data = now.strftime("%d.%m.%Y")
     return new_data
