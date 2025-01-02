cc149dd4aee51aa8f44e99dcb4d75fd8251e2baf
Pavel <shegol164@mail.ru>
2025-01-02 15:02:20 +0400

    add filter_by_state, sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -1,17 +1,17 @@
 def filter_by_state(dictionary):
-    """Функция принимает на вход список словарей и возвращает список отсортированный по значению ключа 'EXECUTED' и 'CANCELED' """
+    """Функция возвращает список отсортированный по значению ключа 'EXECUTED' и 'CANCELED'"""
     new_dictionary_1 = []
     new_dictionary_2 = []
     for i in dictionary:
-        if i['state'] == 'EXECUTED':
+        if i["state"] == "EXECUTED":
             new_dictionary_1.append(i)
     for i in dictionary:
-        if i['state'] == 'CANCELED':
+        if i["state"] == "CANCELED":
             new_dictionary_2.append(i)
-    return f'{new_dictionary_1}\n{new_dictionary_2}'
+    return f"{new_dictionary_1}\n{new_dictionary_2}"
 
 
 def sort_by_date(dictionary):
-    """Функция принимает на вход словарь и сортирует по дате по убыванию """
-    sorted_dictionary = sorted(dictionary, key=lambda x: x['date'], reverse=True)
+    """Функция принимает на вход словарь и сортирует по дате по убыванию"""
+    sorted_dictionary = sorted(dictionary, key=lambda x: x["date"], reverse=True)
     return sorted_dictionary
