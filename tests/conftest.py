64e51ff19294a1a20954d2d4e60823b3812913e4
Pavel <shegol164@mail.ru>
2025-01-15 16:19:17 +0400

    add conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,46 @@
 import pytest
 
 
-
-@pytest. fixture
+@pytest.fixture
 def mask():
     return '1111 11** **** 1111'
-@pytest. fixture
+
+
+@pytest.fixture
+def mask_min():
+    return '1111 11** **** 1111'
+
+
+@pytest.fixture
+def mask_max():
+    return '1111 11** **** 1111'
+
+
+@pytest.fixture
+def mask_zero():
+    return ' ** **** '
+
+
+@pytest.fixture
+def mask_letter():
+    return 'aaaa aa** **** aaaa'
+
+
+@pytest.fixture
+def mask_letter_min():
+    return 'aaaa aa** **** aaaa'
+
+
+@pytest.fixture
+def mask_letter_max():
+    return 'aaaa aa** **** aaaa'
+
+
+@pytest.fixture
 def mask_account():
     return '**1111'
+
+
+@pytest.fixture
+def mask_account_zero():
+    return '**'
