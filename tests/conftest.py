01c29a8b082d9b182770e048b0211aec3b5cb01f
Pavel <shegol164@mail.ru>
2025-01-18 15:15:12 +0400

    add conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -54,3 +54,53 @@ def expected_mask_account_max():
 @pytest.fixture
 def expected_mask_account_zero():
     return '**'
+
+
+@pytest.fixture
+def expected_mask_account_card():
+    return ' Visa 11** **** 1111'
+
+
+@pytest.fixture
+def expected_mask_account_card_min():
+    return ' Visa 11** **** 1111'
+
+
+@pytest.fixture
+def expected_mask_account_card_max():
+    return ' Visa 11** **** 1111'
+
+
+@pytest.fixture
+def expected_mask_account_card_zero():
+    return "Пустая строка"
+
+
+@pytest.fixture
+def expected_mask_account_card_check():
+    return ' **1111'
+
+
+@pytest.fixture
+def expected_mask_account_card_check_min():
+    return ' **1111'
+
+
+@pytest.fixture
+def expected_mask_account_card_check_max():
+    return ' **1111'
+
+
+@pytest.fixture
+def expected_get_date():
+    return '11.03.2024'
+
+
+@pytest.fixture
+def expected_get_date_max():
+    return '11.03.2024'
+
+
+@pytest.fixture
+def expected_get_date_zero():
+    return "Пустая строка"
