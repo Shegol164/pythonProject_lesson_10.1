686f334da0278f8e3bde945714d88632e28e2d29
Pavel <shegol164@mail.ru>
2025-01-21 17:58:08 +0400

    add masks.py

--- src/masks.py
+++ src/masks.py
@@ -3,6 +3,8 @@ def get_mask_card_number(card_number: str) -> str:
     Пример 7000792289606361 # входной аргумент
     7000 79** **** 6361 # выход функции
     """
+    if not card_number:
+        return "Пустая строка"
     return f"{card_number[:4]} {card_number[4:6]}** **** {card_number[-4:]}"
 
 
@@ -12,4 +14,6 @@ def get_mask_account(account: str) -> str:
     73654108430135874305 # входной аргумент
     **4305 # выход функции
     """
+    if not account:
+        return "Пустая строка"
     return f"**{account[-4:]}"
