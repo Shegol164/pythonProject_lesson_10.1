9ca7a6cbe1a1d8e82778cbb21bfdf04f6df9a858
Pavel <shegol164@mail.ru>
2025-01-21 12:34:28 +0400

    add processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,9 +1,11 @@
-from typing import Dict, List
+from typing import Dict, List, Any
 
 
-def filter_by_state(data_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(data_list: List[Dict], state: str = "EXECUTED") -> List[Dict[str,str]]:
     """Принимает список словарей и ключ: state (по умолчанию 'EXECUTED').
     Возвращает новый список словарей, содержащий словари соответствующих ключ"""
+    if not data_list:
+        return []
     new_dictionary = []
     for i in data_list:
         if i["state"] == state:
@@ -11,7 +13,9 @@ def filter_by_state(data_list: List[Dict], state: str = "EXECUTED") -> List[Dict
     return new_dictionary
 
 
-def sort_by_date(data_list: List[Dict], reverse: bool = True) -> List[Dict]:
+def sort_by_date(data_list: List[Dict], reverse: bool = True) -> List[Dict[str,Any]]:
     """Функция принимает список словарей и параметр сортировки(по умолчанию "True" — 'CANCELED').
     Функция возвращает новый список, отсортированный по дате(date)"""
+    if not data_list:
+        return []
     return sorted(data_list, key=lambda x: x["date"], reverse=reverse)
