741a8800a82c8eb57a2bb6f30d5e239cd39c0991
Pavel <shegol164@mail.ru>
2025-02-15 12:20:13 +0400

    Logs have been added to the utils.py module

--- src/utils.py
+++ src/utils.py
@@ -3,8 +3,11 @@ import os
 from pathlib import Path
 from typing import Any
 
+from src.logging import get_logger
+
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path_1 = os.path.join(current_dir, "../logs", "utils.log")
+logger = get_logger("utils", file_path_1)
 
 ROOT_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = ROOT_DIR / "data"
@@ -15,12 +18,15 @@ def operation(open_file: Any) -> Any:
     возвращает список словарей с данными о финансовых транзакциях"""
     open_file += ".json"
     try:
+        logger.info("Данная функция  принимает на вход файл JSON-файла и возвращает список словарей")
         with open(DATA_DIR / open_file, "r", encoding="utf-8") as jf:
             data = json.load(jf)
     except Exception as e:
+        logger.error(f"Произошла ошибка типа {e}")
         print(f"Ошибка {e}")
         return []
     except FileNotFoundError as e:
+        logger.error(f"Произошла ошибка типа {e}")
         print(f"Ошибка {e}")
         return []
     return data
